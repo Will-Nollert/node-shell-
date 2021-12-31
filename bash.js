854c200f41c0c79cebfe227f802d4bd875e63ec4
William Nollert <will.nollert@gmail.com>
2021-12-31 14:14:29 -0600

    separated pwd into a module

--- bash.js
+++ bash.js
@@ -1,15 +1,6 @@
 
-// output a promt 
-process.stdout.write('prompt > ');
 
-//the Stdin data event fires after a user types in a line 
-process.stdin.on('data', (data) => {
-    
-    const cmd = data.toString().trim();//takes "data" makes it strings and aliass it to CMD
-    if(cmd === "pwd"){ // if CMD is the string "pwd" ...
-        process.stdout.write(process.cwd()); //write working directory list
-    } else { //otherwise give old format
-        process.stdout.write( "you wrote " + cmd);
-        }
-    process.stdout.write('\nprompt > ') // reoutputs a promt 
-})
+// access the function in PWD and store ot om a var here 
+const pwd = require('./pwd')
+// if "pwd" is entered as an arg then the function in PWD is called 
+pwd();
---
+++ pwd.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+
+        // output a promt 
+        process.stdout.write('prompt > ');
+
+    process.stdin.on('data', (data) => {
+        const cmd = data.toString().trim();//takes "data" makes it strings and aliass it to CMD
+        if(cmd === "pwd"){ // if CMD is the string "pwd" ...
+            process.stdout.write(process.cwd()); //write working directory list
+        } else { //otherwise give old format
+            process.stdout.write( "you wrote " + cmd);
+            }
+        process.stdout.write('\nprompt > ') // reoutputs a promt 
+    })
+}
